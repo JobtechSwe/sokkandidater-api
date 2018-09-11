71caff7c1bc74230fe1d60c2a4f23c67b327d44c
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-09-11 15:57:17 +0200

    Added ranges for offset and limit

--- sokkandidater/rest/models.py
+++ sokkandidater/rest/models.py
@@ -1,4 +1,4 @@
-from flask_restplus import fields, reqparse
+from flask_restplus import fields, reqparse, inputs
 from sokkandidater.rest import api
 from sokkandidater import settings
 
@@ -22,12 +22,13 @@ resultat_taxonomi = api.model('TaxonomiEntitet', {
 matchande_kandidat = api.model('MatchandeKandidat', {
     'arbetssokandeprofilId': fields.String(attribute='_source.id'),
     'rubrik': fields.String(attribute='_source.rubrik'),
-     'senastModifierad': fields.DateTime(attribute='_source.timestamp'),
+    'senastModifierad': fields.DateTime(attribute='_source.timestamp'),
     'efterfragadArbetsplats': fields.Nested({
         'land': fields.List(fields.Nested(resultat_plats), attribute='krav.land'),
         'lan': fields.List(fields.Nested(resultat_plats), attribute='krav.lan'),
         'kommun': fields.List(fields.Nested(resultat_plats), attribute='krav.kommun'),
-        'geoPosition': fields.List(fields.Nested(resultat_geoposition), attribute='krav.geoPosition')
+        'geoPosition': fields.List(fields.Nested(resultat_geoposition),
+                                   attribute='krav.geoPosition')
     }, attribute='_source', skip_none=True),
     'matchningsresultatKandidat': fields.Nested({
         'efterfragade': fields.Nested({
@@ -50,8 +51,8 @@ kandidat_lista = api.model('Kandidater', {
 
 # Frågemodeller
 sok_kandidat_query = reqparse.RequestParser()
-sok_kandidat_query.add_argument('offset', type=int, default=0)
-sok_kandidat_query.add_argument('limit', type=int, default=10)
+sok_kandidat_query.add_argument('offset', type=inputs.int_range(0, 10000), default=0)
+sok_kandidat_query.add_argument('limit', type=inputs.int_range(0, 200), default=10)
 sok_kandidat_query.add_argument(settings.OCCUPATION, action='append')
 sok_kandidat_query.add_argument(settings.GROUP, action='append')
 sok_kandidat_query.add_argument(settings.FIELD, action='append')
@@ -64,5 +65,7 @@ sok_kandidat_query.add_argument(settings.WORKTIME_EXTENT)
 taxonomy_query = reqparse.RequestParser()
 taxonomy_query.add_argument('q')
 taxonomy_query.add_argument('kod')
-taxonomy_query.add_argument('typ', choices=(settings.OCCUPATION, settings.GROUP, settings.FIELD, settings.SKILL,
-                                            settings.LANGUAGE, settings.MUNICIPALITY, settings.REGION, settings.WORKTIME_EXTENT))
+taxonomy_query.add_argument('typ', choices=(settings.OCCUPATION, settings.GROUP,
+                                            settings.FIELD, settings.SKILL,
+                                            settings.LANGUAGE, settings.MUNICIPALITY,
+                                            settings.REGION, settings.WORKTIME_EXTENT))
