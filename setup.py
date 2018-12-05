2346b99b9095392820dbe171eae95efba000a719
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-12-05 10:02:14 +0100

    Uses find_packages to find packages

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sokkandidater',
-    packages=['sokkandidater'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'valuestore', 'flask', 'flask-restplus', 'flask-cors', 'elasticsearch'
